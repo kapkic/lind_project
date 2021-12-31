1663877713d030c67438789ac3cebe1365fd2b61
Ahmet Kapkic <ahmetkapkic@gmail.com>
2021-12-30 22:07:22 -0500

    Update entrypoint.sh

--- docker-action/entrypoint.sh
+++ docker-action/entrypoint.sh
@@ -5,14 +5,18 @@ git --git-dir /home/lind/lind_project/src/native_client/.git remote update origi
 git --git-dir /home/lind/lind_project/src/safeposix-rust/.git remote update origin --prune;
 git --git-dir /home/lind/lind_project/.git remote update origin --prune;
 
-git --git-dir /home/lind/lind_project/src/lind_glibc/.git checkout remotes/origin/develop;
-git --git-dir /home/lind/lind_project/src/native_client/.git checkout remotes/origin/develop;
-git --git-dir /home/lind/lind_project/src/safeposix-rust/.git checkout remotes/origin/develop;
-git --git-dir /home/lind/lind_project/.git checkout remotes/origin/develop;
+rustup toolchain list
+rustup -V
+
+#disabled for rustposix testing:
+#git --git-dir /home/lind/lind_project/src/lind_glibc/.git checkout remotes/origin/develop;
+#git --git-dir /home/lind/lind_project/src/native_client/.git checkout remotes/origin/develop;
+#git --git-dir /home/lind/lind_project/src/safeposix-rust/.git checkout remotes/origin/develop;
+#git --git-dir /home/lind/lind_project/.git checkout remotes/origin/develop;
 
 make rustposix;  
-make nacl; 
-make glibc; 
-make install;
-make test;
-echo "It's alive!"
+#make nacl; 
+#make glibc; 
+#make install;
+#make test;
+#echo "It's alive!"
