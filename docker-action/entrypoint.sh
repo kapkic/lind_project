46e8094de9161309f7b628464625981c37903045
Ahmet Kapkic <ahmetkapkic@gmail.com>
2021-12-30 21:22:04 -0500

    Update entrypoint.sh

--- docker-action/entrypoint.sh
+++ docker-action/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
-pwd
-tree -d
+git --git-dir /home/lind/lind_project/src/lind_glibc/.git remote update origin --prune;
+git --git-dir /home/lind/lind_project/src/native_client/.git remote update origin --prune;
+git --git-dir /home/lind/lind_project/src/safeposix-rust/.git remote update origin --prune;
+git --git-dir /home/lind/lind_project/.git remote update origin --prune;
+
+git --git-dir /home/lind/lind_project/src/lind_glibc/.git checkout remotes/origin/develop;
+git --git-dir /home/lind/lind_project/src/native_client/.git checkout remotes/origin/develop;
+git --git-dir /home/lind/lind_project/src/safeposix-rust/.git checkout remotes/origin/develop;
+git --git-dir /home/lind/lind_project/.git checkout remotes/origin/develop;
+
+make rustposix;  
+make nacl; 
+make glibc; 
+make install;
+make test;
+echo "It's alive!"
