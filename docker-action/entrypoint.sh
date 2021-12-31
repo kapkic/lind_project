8c44167b29f6d0f20c649e9ee3e3bcca4a55ff2b
Ahmet Kapkic <ahmetkapkic@gmail.com>
2021-12-31 14:22:03 -0500

    Update entrypoint.sh

--- docker-action/entrypoint.sh
+++ docker-action/entrypoint.sh
@@ -5,29 +5,32 @@ git --git-dir /home/lind/lind_project/src/native_client/.git remote update origi
 git --git-dir /home/lind/lind_project/src/safeposix-rust/.git remote update origin --prune;
 git --git-dir /home/lind/lind_project/.git remote update origin --prune;
 
-echo "Toolchain:"
-rustup toolchain list
-echo "Rustup:"
-rustup -V #rust toolchain is not installed?
-echo "Cargo:"
-cargo -V
+#echo "Toolchain:"
+#rustup toolchain list
+#echo "Rustup:"
+#rustup -V #rust toolchain is not installed?
+#echo "Cargo:"
+#cargo -V
 
 rustup toolchain install nightly-2021-09-27 --allow-downgrade --profile minimal --component clippy
 
-echo "Toolchain:"
-rustup toolchain list
-echo "Rustup:"
-rustup -V #rust toolchain is not installed?
-echo "Cargo:"
-cargo -V
-ls
-pwd
+#echo "Toolchain:"
+#rustup toolchain list
+#echo "Rustup:"
+#rustup -V #rust toolchain is not installed?
+#echo "Cargo:"
+#cargo -V
+#ls
+#pwd
+
 cp helloX.nexe /home/lind/lind_project/helloX.nexe
 cd /home/lind/lind_project/
 ls
 pwd
 
-lindfs cp /home/lind/lind_project/helloX.nexe /lind/helloX.nexe
+lindfs cp /home/lind/lind_project/helloY.nexe /lind/helloY.nexe
+lind -vvvv /lind/helloY.nexe
+echo "blank"
 lind -vvvv /lind/helloINVALID.nexe
 
 
@@ -38,13 +41,13 @@ lind -vvvv /lind/helloINVALID.nexe
 #git --git-dir /home/lind/lind_project/.git checkout remotes/origin/develop;
 
 echo "In the land of RUSTPOSIX where the shadows lie:"
-#make rustposix;  
+make rustposix;  
 echo "One NACL to rule them all:"
-#make nacl; 
+make nacl; 
 echo "One GLIBC to find them:"
-#make glibc; 
+make glibc; 
 echo "One ring to INSTALL them all"
-#make install;
+make install;
 echo "And in darkness TEST them"
 #make test-verbose;
 #echo "It's alive!"
